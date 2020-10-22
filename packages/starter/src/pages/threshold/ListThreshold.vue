837a604a7a34a3d347d276df440421aae3fe11f1
selen96 <selen96ivan@yandex.ru>
2020-10-22 12:37:38 -0700

    threshold list page

--- packages/starter/src/pages/threshold/ListThreshold.vue
+++ packages/starter/src/pages/threshold/ListThreshold.vue
@@ -1,81 +1,177 @@
 <template>
-  <div></div>
+  <div class="d-flex flex-column flex-grow-1">
+    <div class="d-flex align-center py-3">
+      <div>
+        <div class="display-1">Thresholds</div>
+        <!-- <v-breadcrumbs :items="breadcrumbs" class="pa-0 py-2"></v-breadcrumbs> -->
+      </div>
+    </div>
+    <v-card>
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        hide-default-footer
+      >
+        <template v-slot:item.id="{ item }">
+          <div class="font-weight-bold"># <copy-label :text="item.id" /></div>
+        </template>
+
+        <template v-slot:item.date="{ item }">
+          <div>{{ item.date | formatDate }}</div>
+        </template>
+
+        <template v-slot:item.companyMail="{ item }">
+          <v-icon v-if="item.companyMail.status" color="green">mdi-check</v-icon>
+          {{ item.companyMail.email }}
+        </template>
+
+        <template v-slot:item.sms="{ item }">
+          <v-icon v-if="item.sms.status" color="green">mdi-check</v-icon>
+          {{ item.sms.sms }}
+        </template>
+
+        <template v-slot:item.status="{ item }">
+          <div class="font-weight-bold d-flex align-center text-no-wrap">
+            <div v-if="item.status" class="warning--text">
+              <v-btn
+                color="success"
+                @click="item.status = !item.status"
+              >Enabled</v-btn>
+            </div>
+            <div v-else class="success--text">
+              <v-btn
+                color="warning"
+                @click="item.status = !item.status"
+              >Disabled</v-btn>
+            </div>
+          </div>
+        </template>
+
+        <template v-slot:item.action="{ item }">
+          <div class="actions">
+            <v-btn icon @click="open(item)">
+              <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+          </div>
+        </template>
+      </v-data-table>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import CopyLabel from '../../components/common/CopyLabel'
+
+/*
+|---------------------------------------------------------------------
+| Threshold List Page
+|---------------------------------------------------------------------
+|
+| List all thresholds
+|
+*/
 export default {
+  components: {
+    CopyLabel
+  },
   props: {
-    user: {
-      type: Object,
-      default: () => ({})
-    },
-    roles: {
-      type: Array,
-      default: () => []
+    label: {
+      type: String,
+      default: ''
     }
   },
-  data() {
+  data () {
     return {
-      products: [
-        'BD Batch Blender',
-        'Accumeter Ovation Continuous',
-        'GH Gravimetric Extrusion Control',
-        'GH-F Gravimetric Additive',
-        'VTC Plus Conveying',
-        'NGX',
-        'NGX Nomad',
-        'Truetemp',
-        'GP & HE Central',
-        'T50 Central'
+      headers: [
+        { text: 'Id', align: 'start', value: 'id' },
+        { text: 'Condition', sortable: false, value: 'condition' },
+        { text: 'Email', value: 'companyMail' },
+        { text: 'Date', value: 'date' },
+        { text: 'SMS', value: 'sms' },
+        { text: 'Status', value: 'status' },
+        { text: '', sortable: false, align: 'right', value: 'action' }
       ],
-      telemetries: [
-        'Power Loss',
-        'Out of Material',
-        'Hopper Unstable',
-        'Hopper Overfeed',
-        'Hopper Over Max',
-        'Max Empty Weight',
-        'Mixer Failure',
-        'Unable to make rate',
-        'Pump Starter Fault'
-      ],
-      operators: [
-        'Equals',
-        'Does not equal',
-        'Is greater than',
-        'Is greater than or equal to',
-        'Is less than',
-        'Is less than or equal to',
-        'Is in',
-        'Is not in'
-      ],
-      switch1: true,
-
-      defaultFilter: {
-        telemetry: '',
-        operator: '',
-        value: ''
-      },
-      filters: [
+      items: [
         {
-          telemetry: '',
-          operator: '',
-          value: ''
+          id: '2837',
+          condition: 'Threshold condition name',
+          companyMail: {
+            email: 'johnsimon@blobhill.com',
+            status: true
+          },
+          date: '2020-05-10',
+          company: 'BlobHill',
+          sms: {
+            sms: '5281224377',
+            status: true
+          },
+          status: 'true'
+        },
+        {
+          id: '2838',
+          condition: 'Threshold condition name',
+          companyMail: {
+            email: 'cool@caprimooner.com',
+            status: false
+          },
+          date: '2020-05-11',
+          company: 'Caprimooner',
+          sms: {
+            sms: '212122433',
+            status: false
+          },
+          status: 'true'
+        },
+        {
+          id: '2839',
+          condition: 'Threshold condition name',
+          companyMail: {
+            email: 'bush@catloveisstilllove.com',
+            status: false
+          },
+          date: '2020-05-11',
+          company: 'CatLovers',
+          sms: {
+            sms: '1231224313',
+            status: false
+          },
+          status: 'true'
+        },
+        {
+          id: '2840',
+          condition: 'Threshold condition name',
+          companyMail: {
+            email: 'ben@indiecoolers.com',
+            status: false
+          },
+          date: '2020-05-12',
+          company: 'IndieCoolers',
+          sms: {
+            sms: '987122433',
+            status: true
+          },
+          status: 'true'
+        },
+        {
+          id: '2841',
+          condition: 'Threshold condition name',
+          companyMail: {
+            email: 'jack@candylooove.com',
+            status: true
+          },
+          date: '2020-05-13',
+          company: 'CandyLooove',
+          sms: {
+            sms: '2951224373',
+            status: true
+          },
+          status: 'true'
         }
-      ],
-      emailForm: {
-        name: '',
-        to: '',
-        note: '',
-        media: []
-      },
-      panel: [0]
+      ]
     }
   },
   methods: {
-    addFilter() {
-      this.filters.push(this.defaultFilter)
-    }
+    open(item) { }
   }
 }
 </script>
