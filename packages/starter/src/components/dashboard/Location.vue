b83307449dc29c863475e78710d4ca9a84608968
selen96 <selen96ivan@yandex.ru>
2020-11-04 11:26:49 -0800

    dashboard page update

--- packages/starter/src/components/dashboard/Location.vue
+++ packages/starter/src/components/dashboard/Location.vue
@@ -43,34 +43,36 @@
           </v-col>
           <v-col xs="12" sm="6" md="4">
             <v-card outlined height="300">
-              <v-card-text class="d-flex">
-                <div>
-                  <div class="title text-center">Cycle Time</div>
-                  <vue-speedometer
-                    :width="200"
-                    :height="140"
-                    :ringWidth="25"
-                    :needleHeightRatio="0.7"
-                    :maxSegmentLabels="0"
-                    :segments="3"
-                    :customSegmentStops="[0, 900, 1000]"
-                    :segmentColors='["#AAA", "#092954"]'
-                    needleColor="#092954"
-                    :value="900"
-                    currentValueText="40"
-                    style="text-align: center"
-                    :forceRender="true"
-                  />
-                  <div class="title text-center">
-                    Actual Target
-                    <v-icon color="green" style="font-size: 50px;">mdi-menu-down</v-icon>
+              <v-card-text>
+                <div class="title text-center">Cycle Time</div>
+                <div class="d-flex">
+                  <div>
+                    <vue-speedometer
+                      :width="200"
+                      :height="140"
+                      :ringWidth="25"
+                      :needleHeightRatio="0.7"
+                      :maxSegmentLabels="0"
+                      :segments="3"
+                      :customSegmentStops="[0, 900, 1000]"
+                      :segmentColors='["#AAA", "#092954"]'
+                      needleColor="#092954"
+                      :value="900"
+                      currentValueText="40"
+                      style="text-align: center"
+                      :forceRender="true"
+                    />
+                    <div class="title text-center">
+                      Actual Target
+                      <v-icon color="green" style="font-size: 50px;">mdi-menu-down</v-icon>
+                    </div>
                   </div>
-                </div>
-                <div class="">
-                  <div class="text-right">
-                    <div class="title">6.18</div>
-                    <div class="title">6.20</div>
-                    <div class="display-1">-0.02</div>
+                  <div class="">
+                    <div class="text-right">
+                      <div class="title">6.18</div>
+                      <div class="title">6.20</div>
+                      <div class="display-1">-0.02</div>
+                    </div>
                   </div>
                 </div>
               </v-card-text>
