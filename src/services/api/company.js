19cf2e33287288f2852ed8e65619446464a168d1
selen96 <selen96ivan@yandex.ru>
2020-11-13 12:41:48 -0800

    company service api added and axios calls from customer module are moved to it

---
+++ src/services/api/company.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+export default {
+  getCustomers() {
+    return axios.get('/customers').then((response) => {
+      return response
+    })
+  },
+  addCustomer(companyName, administratorName, administratorEmail) {
+    const data = {
+      company_name: companyName,
+      administrator_name: administratorName,
+      administrator_email: administratorEmail
+    }
+
+    return axios.post('/customers/add', data).then((response) => {
+      return response
+    })
+  },
+  getCustomer(id) {
+    return axios.get(`/customers/${id}`).then((response) => {
+      return response
+    })
+  },
+  updateAccount(accountInfo) {
+    return axios.post(`/customers/update-account/${accountInfo.id}`, {
+      name: accountInfo.name,
+      administrator_name: accountInfo.administratorName,
+      administrator_email: accountInfo.administratorEmail
+    }).then((response) => {
+      return response
+    })
+  },
+  updateProfile(data) {
+    return axios.post(`/customers/update-profile/${data.id}`, {
+      address_1: data.address_1,
+      address_2: data.address_2,
+      zip: data.zip,
+      state: data.state,
+      city: data.city,
+      country: data.country,
+      phone: data.phone
+    }).then((response) => {
+      return response
+    })
+  }
+}
--- src/store/customers/index.js
+++ src/store/customers/index.js
@@ -1,3 +1,5 @@
+import companyAPI from '@/services/api/company'
+
 const module = {
   namespaced: true,
   state: {
@@ -12,12 +14,9 @@ const module = {
     getCustomers({
       commit
     }) {
-      this.$axios.get('/customers')
-        .then((response) => {
-          commit('SET_CUSTOMERS', response.data.companies)
-        })
-        .catch((error) => {
-        })
+      companyAPI.getCustomers().then((response) => {
+        commit('SET_CUSTOMERS', response.data.companies)
+      })
     },
     addCustomer({
       commit, dispatch
@@ -25,30 +24,23 @@ const module = {
       companyName, administratorName, administratorEmail
     }) {
       commit('BUTTON_LOAD')
-      const data = {
-        company_name: companyName,
-        administrator_name: administratorName,
-        administrator_email: administratorEmail
-      }
-
-      this.$axios.post('/customers/add', data)
-        .then((response) => {
-          commit('BUTTON_CLEAR')
-          dispatch('app/showSuccess', response.data, { root: true })
-        })
+      companyAPI.addCustomer(companyName, administratorName, administratorEmail).then((response) => {
+        dispatch('app/showSuccess', response.data, { root: true })
+      })
         .catch((error) => {
-          commit('BUTTON_CLEAR')
           console.log(error.response.data)
+        })
+        .finally(() => {
+          commit('BUTTON_CLEAR')
         })
     },
     getCustomer({
       commit
     }, id) {
-      this.$axios.get(`/customers/${id}`)
-        .then((response) => {
-          commit('SET_CUSTOMER_ACCOUNT', response.data.company)
-          commit('SET_CUSTOMER_PROFILE', response.data.profile)
-        })
+      companyAPI.getCustomer(id).then((response) => {
+        commit('SET_CUSTOMER_ACCOUNT', response.data.company)
+        commit('SET_CUSTOMER_PROFILE', response.data.profile)
+      })
         .catch((error) => {
           console.log(error.response.data)
         })
@@ -57,18 +49,10 @@ const module = {
       commit, dispatch
     }, account) {
       commit('BUTTON_LOAD')
-      this.$axios.post(`/customers/update-account/${account.id}`, {
-        name: account.name,
-        administrator_name: account.administratorName,
-        administrator_email: account.administratorEmail
+      companyAPI.updateAccount(account).then((response) => {
+        dispatch('app/showSuccess', response.data, { root: true })
       })
-        .then((response) => {
-          dispatch('app/showSuccess', response.data, { root: true })
-          commit('BUTTON_CLEAR')
-          // commit('SET_CUSTOMER', response.data.company)
-        })
         .catch((error) => {
-          commit('BUTTON_CLEAR')
           if (error.response.status === 422) {
             const errors = Object.values(error.response.data.error).flat()
 
@@ -77,29 +61,19 @@ const module = {
             })
           }
         })
+        .finally(() => {
+          commit('BUTTON_CLEAR')
+        })
     },
     updateProfile({
       commit, dispatch
     }, data) {
       commit('BUTTON_LOAD')
-      this.$axios.post(`/customers/update-profile/${data.id}`, {
-        address_1: data.address_1,
-        address_2: data.address_2,
-        zip: data.zip,
-        state: data.state,
-        city: data.city,
-        country: data.country,
-        phone: data.phone
+      companyAPI.updateProfile(data).then((response) => {
+        dispatch('app/showSuccess', response.data, { root: true })
       })
-        .then((response) => {
+        .finally(() => {
           commit('BUTTON_CLEAR')
-          dispatch('app/showSuccess', response.data, { root: true })
-          // console.log(response.data)
-          // commit('SET_CUSTOMER', response.data.company)
-        })
-        .catch((error) => {
-          commit('BUTTON_CLEAR')
-          // console.log(error.response.data)
         })
     },
     clearError({ commit }) {
