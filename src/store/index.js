28f7866ffee91688576e526f6d27749132bf67fd
selen96 <selen96ivan@yandex.ru>
2020-11-25 23:53:00 +0800

    table loading status added to users and customers page

--- src/store/index.js
+++ src/store/index.js
@@ -18,8 +18,8 @@ Vue.use(Vuex)
 
 const axios = require('axios')
 
-// axios.defaults.baseURL = process.env.VUE_APP_SERVER_API_ENDPOINT
-axios.defaults.baseURL = 'http://localhost/acs-api/public/api'
+axios.defaults.baseURL = process.env.VUE_APP_SERVER_API_ENDPOINT
+// axios.defaults.baseURL = 'http://localhost/acs-api/public/api'
 // axios.defaults.baseURL = 'http://157.230.210.3/api'
 
 axios.interceptors.request.use(
