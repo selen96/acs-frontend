d3c6cef6764d803c0ce54debb3c86cbc6d318beb
selen96 <selen96ivan@yandex.ru>
2021-01-23 17:09:16 +0800

    blender recipe component updated - Do Data From Device case added

--- src/components/dashboard/product/product-analytics/bd-batch-blender/components/Recipe.vue
+++ src/components/dashboard/product/product-analytics/bd-batch-blender/components/Recipe.vue
@@ -64,6 +64,9 @@ export default {
             return [val.toFixed(2) + '%']
           }
         },
+        noData: {
+          text: 'No Data From Devce'
+        },
         legend: {
           show: true,
           position: 'bottom',
