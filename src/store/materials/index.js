1f766362400233c6156ede1df84ac68fe05951d0
selen96 <selen96ivan@yandex.ru>
2021-05-04 20:16:13 +0300

    [new_system_inventory_report] fix2 error message

--- src/store/materials/index.js
+++ src/store/materials/index.js
@@ -230,7 +230,10 @@ const module = {
 
         commit('SET_SYSTEM_INVENTORY_REPORTS', Object.keys(response.keyed_materials).map((r) => response.keyed_materials[r]))
       } catch (error) {
-        dispatch('app/showError', { message: 'Failed: ', error: { message: 'Please check your time range selection' } }, { root: true })
+        dispatch('app/showError', {
+          message: 'Failed: ',
+          error: { message: error }
+        }, { root: true })
 
         console.log(error)
         throw error
